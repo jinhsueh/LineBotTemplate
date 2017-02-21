17ac47af94e10ee118549024061f5413fb10ee2c
jinhsueh <hsuehchin76@gmail.com>
2017-02-21 18:14:27 +0800

    First commit

--- main.go
+++ main.go
@@ -56,3 +56,31 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 }
+{
+  "type": "template",
+  "altText": "this is a buttons template",
+  "template": {
+      "type": "buttons",
+      "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
+      "title": "Menu",
+      "text": "Please select",
+      "actions": [
+          {
+            "type": "postback",
+            "label": "Buy",
+            "data": "action=buy&itemid=123"
+          },
+          {
+            "type": "postback",
+            "label": "Add to cart",
+            "data": "action=add&itemid=123"
+          },
+          {
+            "type": "uri",
+            "label": "View detail",
+            "uri": "http://example.com/page/123"
+          }
+      ]
+  }
+}
+
